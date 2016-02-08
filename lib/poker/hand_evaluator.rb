75aba818768275e181578afc81ec72a1049dc85c
Thiago A. Silva <thiagoaraujos@gmail.com>
2016-02-08 16:23:15 -0300

    Move file to correct folder

--- lib/poker/hand_evaluator.rb
+++ lib/poker/hand_evaluator.rb
@@ -1,21 +1,19 @@
-module Poker
-  class HandEvaluator
-    include Factory
+class HandEvaluator
+  include Poker::Factory
 
-    def return_stronger_hand(raw_left_hand, raw_right_hand)
-      left_hand, right_hand = build_hands(raw_left_hand, raw_right_hand)
+  def return_stronger_hand(raw_left_hand, raw_right_hand)
+    left_hand, right_hand = build_hands(raw_left_hand, raw_right_hand)
 
-      if left_hand > right_hand
-        raw_left_hand
-      else
-        raw_right_hand
-      end
+    if left_hand > right_hand
+      raw_left_hand
+    else
+      raw_right_hand
     end
+  end
 
-    private
+  private
 
-    def build_hands(*raw_hands)
-      raw_hands.map { |raw_hand| build_hand(raw_hand).call }
-    end
+  def build_hands(*raw_hands)
+    raw_hands.map { |raw_hand| build_hand(raw_hand).call }
   end
 end
