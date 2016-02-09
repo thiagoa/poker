07e1a85b6ef918911889743a50948f1dcaab5a29
Thiago A. Silva <thiagoaraujos@gmail.com>
2016-02-09 15:07:17 -0300

    Put HandEvaluator in correct namespace

--- lib/poker/hand_evaluator.rb
+++ lib/poker/hand_evaluator.rb
@@ -1,23 +1,25 @@
-class HandEvaluator
-  include Poker::Factory, Poker::HandGrouper
+module Poker
+  class HandEvaluator
+    include Poker::Factory, Poker::HandGrouper
 
-  def initialize
-    @hand_groupers = Collection.new(traditional_high_poker)
-  end
-
-  def return_stronger_hand(raw_left_hand, raw_right_hand)
-    left_hand, right_hand = build_hands(raw_left_hand, raw_right_hand)
-
-    if left_hand > right_hand
-      raw_left_hand
-    else
-      raw_right_hand
+    def initialize
+      @hand_groupers = Collection.new(traditional_high_poker)
     end
-  end
 
-  private
+    def return_stronger_hand(raw_left_hand, raw_right_hand)
+      left_hand, right_hand = build_hands(raw_left_hand, raw_right_hand)
 
-  def build_hands(*raw_hands)
-    raw_hands.map { |raw_hand| build_hand(raw_hand, @hand_groupers).call }
+      if left_hand > right_hand
+        raw_left_hand
+      else
+        raw_right_hand
+      end
+    end
+
+    private
+
+    def build_hands(*raw_hands)
+      raw_hands.map { |raw_hand| build_hand(raw_hand, @hand_groupers).call }
+    end
   end
 end
